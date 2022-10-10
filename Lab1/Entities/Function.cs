4a98589268e7270114179175859a0bd89c9be75f
Ar4eR-ValerA <ar4er.valera@gmail.com>
2022-10-10 12:33:33 +0300

    feat: function ans system of equations

---
+++ Lab1/Entities/Function.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab1.Entities;
+
+public class Function
+{
+    private Dictionary<int, double> _coefficients;
+
+    public Function(Dictionary<int, double> coefficients)
+    {
+        _coefficients = coefficients;
+    }
+
+    public IReadOnlyDictionary<int, double> Coefficients => _coefficients;
+}
