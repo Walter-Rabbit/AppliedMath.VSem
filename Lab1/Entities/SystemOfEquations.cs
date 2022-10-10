9390e76074e47bfb9b1951da04e62ed374f13eff
Valery Shevchenko <ar4er.valera@gmail.com>
2022-10-10 19:12:05 +0300

    chore: remove simplex method from SystemOfEquations.cs

--- Lab1/Entities/SystemOfEquations.cs
+++ Lab1/Entities/SystemOfEquations.cs
@@ -10,26 +10,4 @@ public class SystemOfEquations
     }
 
     public IReadOnlyList<Equation> Equations => _equations;
-
-    /// <summary>
-    /// Минимизирует функцию с ограничениями, заданными системой уравнений.
-    /// </summary>
-    /// <param name="function">Целевая функция для минимизации.</param>
-    /// <returns>Значения x, при которых функция минимальна.</returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Dictionary<int, double> Minimize(Function function)
-    {
-        throw new NotImplementedException();
-    }
-    
-    /// <summary>
-    /// Максимизирует функцию с ограничениями, заданными системой уравнений.
-    /// </summary>
-    /// <param name="function">Целевая функция для максимизации.</param>
-    /// <returns>Значения x, при которых функция максимальна.</returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public Dictionary<int, double> Maximize(Function function)
-    {
-        throw new NotImplementedException();
-    }
 }
