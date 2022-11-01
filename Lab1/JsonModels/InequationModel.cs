bafab9549766d702257f8b4c88704f77f95a93fc
Valery Shevchenko <ar4er.valera@gmail.com>
2022-11-01 16:42:31 +0300

    feat: file input and output

---
+++ Lab1/JsonModels/InequationModel.cs
@@ -0,0 +1,16 @@
+﻿using Lab1.Entities;
+using Lab1.Entities.Enums;
+
+namespace Lab1.JsonModels;
+
+public class InequalityModel
+{
+    public List<double> Coefficients { get; set; }
+    public double FreeElement { get; set; }
+    public Term Term { get; set; }
+
+    public ILimitation GetEntity()
+    {
+        return new Inequality(new Expression(Coefficients), FreeElement, Term);
+    }
+}
---
+++ Lab1/JsonModels/SimplexTaskModel.cs
@@ -0,0 +1,19 @@
+﻿using Lab1.Entities;
+
+namespace Lab1.JsonModels;
+
+public class SimplexTaskModel
+{
+    public List<EquationModel> Equations { get; set; }
+    public List<InequalityModel> Inequalities { get; set; }
+    public FunctionModel Function { get; set; }
+    public Mode Mode { get; set; }
+
+    public (SystemOfLimitations, Expression) GetEntity()
+    {
+        var limitations = new List<ILimitation>();
+        limitations.AddRange(Equations.Select(e => e.GetEntity()));
+        limitations.AddRange(Inequalities.Select(i => i.GetEntity()));
+        return (new SystemOfLimitations(limitations), Function.GetEntity());
+    }
+}
